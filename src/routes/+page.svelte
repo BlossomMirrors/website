526c64f9681a1e1eae681f261037a649b3ebe673
koyu <me@koyu.space>
2026-05-20 03:54:10 +0200

    Add noise grain image and BlossomOS landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages';
+</script>
+
+<div class="container">
+	<img src="/noise.png" alt="noise" class="mb-4 rounded-2xl" />
+	<h1 class="font-serif text-8xl">BlossomOS</h1>
+	<p>{m.home_subtitle()}</p>
+</div>
